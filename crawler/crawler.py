61aaa03d27841770e9e855afa17805625f2d3a78
Kritika <kritika@pop-os.localdomain>
2021-03-30 18:45:01 +0530

    extracting title,desciption and url

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,19 +1,20 @@
 
 #creating a simple web crawler
 import requests
+import json
 from bs4 import BeautifulSoup
 
 
 start_url = 'https://stackoverflow.com/'
 
 def crawl(url):
-	print('Crawl("%s")' % url)
 
 	response = requests.get(start_url)
 	content = BeautifulSoup(response.text,'lxml')
 
 	links = content.findAll('a')
-	description = content.findAll('p')
+	title = content.find('title').text
+	description = content.find('p').text 
 
 
 	for link in links:
@@ -22,4 +23,11 @@ def crawl(url):
 		except KeyError:
 			pass
 
-crawl(start_url)
+	return {
+	      'url' : url,
+	      'title' : title,
+	      'description' : description
+	}
+
+result = crawl(start_url)
+print(json.dumps(result,indent=2))
