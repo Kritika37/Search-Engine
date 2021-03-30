09bb65ff7ec8240c1e4236940317b31bf844b045
Kritika <kritika@pop-os.localdomain>
2021-03-30 19:14:50 +0530

    giving links with depth and other # links too ,needed to be fixed

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -7,27 +7,36 @@ from bs4 import BeautifulSoup
 
 start_url = 'https://stackoverflow.com/'
 
-def crawl(url):
+def crawl(url,depth):
 
 	response = requests.get(start_url)
 	content = BeautifulSoup(response.text,'lxml')
 
-	links = content.findAll('a')
+	
 	title = content.find('title').text
-	description = content.find('p').text 
+	description = content.find('p').text.strip().replace('\n', '')
 
-
-	for link in links:
-		try:
-			pass
-		except KeyError:
-			pass
-
-	return {
+	result = {
 	      'url' : url,
 	      'title' : title,
 	      'description' : description
 	}
 
-result = crawl(start_url)
+	if depth == 0:
+		return result
+
+	try:
+		links = content.findAll('a')
+	except:
+		return result
+
+	for link in links:
+		try:
+			print('following url: "%s"' % link['href'])
+		except KeyError:
+			pass
+
+	return result
+
+result = crawl(start_url,1)
 print(json.dumps(result,indent=2))
